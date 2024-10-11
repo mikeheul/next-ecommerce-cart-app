e4efe0e5c95eefae61fd34a228576ff0cc6da7d0
Micka <mickael.murmann@gmail.com>
2024-10-11 09:31:31 +0200

    Fix CSS Navbar (bg)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
     const totalItems = getTotalItems(); // Get the total items count from the context
 
     return (
-        <nav className="bg-gray-800 p-4">
+        <nav className="bg-gray-900 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/products" className="text-white text-lg font-bold">
                     E-commerce
